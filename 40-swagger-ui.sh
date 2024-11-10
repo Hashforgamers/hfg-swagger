125775a210c7be9d3ba3ce8ce6f26ee53433d47b
Hashforgamers <hashforgamers@gmail.com>
2024-11-10 13:49:21 +0100

    Delete 40-swagger-ui.sh

--- 40-swagger-ui.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# /docker-entrypoint.d/40-swagger-ui.sh
-
-# Check if swagger.json already exists before trying to copy
-if [ ! -f /usr/share/nginx/html/swagger.json ]; then
-  cp /swagger.json /usr/share/nginx/html/swagger.json
-else
-  echo "swagger.json already exists, skipping copy"
-fi
