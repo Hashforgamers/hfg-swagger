25cd413ac22b10c25c639680af652ca9b3694b5b
Hashforgamers <hashforgamers@gmail.com>
2024-11-10 17:01:51 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,17 +1,15 @@
 from flask import Flask, send_from_directory
-from werkzeug.utils import safe_join
 from flask_swagger_ui import get_swaggerui_blueprint
-import os
 
 app = Flask(__name__)
 
 # External URL for the swagger.json file
-API_URL = 'https://hfg-swagger.onrender.com/swagger.json'
+SWAGGER_JSON_URL = 'https://hfg-swagger.onrender.com/swagger.json'
 
 # Set up Swagger UI Blueprint
 swaggerui_blueprint = get_swaggerui_blueprint(
     '/swagger',  # The URL path to access Swagger UI
-    API_URL,  # External URL for the swagger.json file
+    SWAGGER_JSON_URL,  # External URL for the swagger.json file
     config={
         'app_name': "My Flask API"
     }
@@ -21,10 +19,9 @@ swaggerui_blueprint = get_swaggerui_blueprint(
 app.register_blueprint(swaggerui_blueprint, url_prefix='/swagger')
 
 # Serve the raw swagger.json (optional, in case you need to serve it locally too)
-@app.route('/swagger.json', methods=['GET'])
+@app.route('/swagger.json')
 def swagger_json():
-    directory = safe_join(app.root_path, 'swagger')
-    return send_from_directory(directory, 'swagger.json')
+    return send_from_directory('swagger', 'swagger.json')  # Optional: serve it locally
 
 if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=5000, debug=True)  # Specify port=5050
+    app.run(debug=True)
