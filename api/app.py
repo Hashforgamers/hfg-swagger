1648917ed7a4c11dccdb8339a15ac2c00100b8a8
Hashforgamers <hashforgamers@gmail.com>
2024-11-10 14:15:52 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -5,11 +5,13 @@ app = Flask(__name__)
 
 # Define the external URL for the swagger.json file
 SWAGGER_JSON_URL = 'https://hfg-swagger.onrender.com/swagger.json'  # Change this to your external URL
+SWAGGER_JSON = 'https://hfg-swagger.onrender.com/swagger.json' 
 
 # Set up Swagger UI Blueprint
 swaggerui_blueprint = get_swaggerui_blueprint(
     '/swagger',  # The URL path to access Swagger UI
     SWAGGER_JSON_URL,  # External URL for the swagger.json file
+    SWAGGER_JSON,
     config={
         'app_name': "My Flask API"
     }
